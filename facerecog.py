a19a40c0830fe708d9e5d28f5f68ebdc9bdb5051
tonyeko <tonyeko@users.noreply.github.com>
2019-10-25 19:38:03 +0700

    Add: Euclidean Distance, tapi print Similarity nya belum

--- facerecog.py
+++ facerecog.py
@@ -36,12 +36,13 @@ def extract_features(image_path, vector_size=32):
     return dsc
 
 def norm(vector):
-    norm = math.sqrt(sum(i**2 for i in vector))
-    return norm
+    return math.sqrt(sum(i**2 for i in vector))
 
-# def eucledian_dist():
-
-#     return 
+def eucledian_dist(sample, test_data):
+    sample_vector = extract_features(sample)
+    test_data_vector = extract_features(test_data)
+    eucdist = math.sqrt(sum((sample_vector[i] - test_data_vector[i])**2 for i in range(len(sample_vector))))
+    return test_data, eucdist
 
 def cos_sim(sample, test_data):
     sample_vector = extract_features(sample)
@@ -54,13 +55,25 @@ def main():
     datauji_path = 'datauji/'
     datauji = [os.path.join(datauji_path, p) for p in sorted(os.listdir(datauji_path))]
     # sample = input("Masukkan nama file: ")
-    # METODE COSINE SIMILARITY
-    cos_sim_arr = []
     sample = os.path.join(datauji_path,"taylor swift4.jpg")
+    
+    # METODE COSINE SIMILARITY
+    # cos_sim_arr = []
+    # for i in datauji:
+    #     cos_sim_arr.append(cos_sim(sample, i))    
+    # cos_sim_arr.sort(key=lambda tup:tup[1], reverse=True)
+    # for i in cos_sim_arr:
+    #     print("Similarity:", round(i[1], 5))
+    #     img = cv2.imread(i[0])
+    #     cv2.imshow('image', img)
+    #     cv2.waitKey(0)
+
+    # METODE EUCLIDEAN DISTANCE
+    euc_dist_arr = []
     for i in datauji:
-        cos_sim_arr.append(cos_sim(sample, i))    
-    cos_sim_arr.sort(key=lambda tup:tup[1], reverse=True)
-    for i in cos_sim_arr:
+        euc_dist_arr.append(eucledian_dist(sample, i))   
+    euc_dist_arr.sort(key=lambda tup:tup[1], reverse=False)
+    for i in euc_dist_arr:
         print("Similarity:", round(i[1], 5))
         img = cv2.imread(i[0])
         cv2.imshow('image', img)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# import sample
 import facerecog
 import os
 
 facerecog.main()
 
 # facerecog.printmenu()
-# sample.run()
