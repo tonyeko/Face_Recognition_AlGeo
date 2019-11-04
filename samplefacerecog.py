5ccefe732d2051241c9792e441b7c538f6a6d4c1
tonyeko <tonyeko@users.noreply.github.com>
2019-11-04 21:52:57 +0700

    bikin extractor.py

--- samplefacerecog.py
+++ samplefacerecog.py
@@ -1,54 +1,10 @@
 import cv2
 import numpy as np
 import scipy
-from scipy.misc import imread
 import _pickle as pickle
-import random
 import os
 import math
-
-# Feature extractor
-def extract_features(image_path, vector_size=32):
-    image = cv2.imread(image_path)
-    try:
-        # Using KAZE, cause SIFT, ORB and other was moved to additional module
-        # which is adding addtional pain during install
-        alg = cv2.KAZE_create()
-        # Dinding image keypoints
-        kps = alg.detect(image)
-        # Getting first 32 of them. 
-        # Number of keypoints is varies depend on image size and color pallet
-        # Sorting them based on keypoint response value(bigger is better)
-        kps = sorted(kps, key=lambda x: -x.response)[:vector_size]
-        # computing descriptors vector
-        kps, dsc = alg.compute(image, kps)
-        # Flatten all of them in one big vector - our feature vector
-        dsc = dsc.flatten()
-        # Making descriptor of same size
-        # Descriptor vector size is 64
-        needed_size = (vector_size * 64)
-        if dsc.size < needed_size:
-            # if we have less the 32 descriptors then just adding zeros at the
-            # end of our feature vector
-            dsc = np.concatenate([dsc, np.zeros(needed_size - dsc.size)])
-    except cv2.error as e:
-        print ('Error: ', e)
-        return None
-
-    return dsc
-
-def batch_extractor(images_path, pickled_db_path="datauji.pck"):
-    files = [os.path.join(images_path, p) for p in sorted(os.listdir(images_path))]
-
-    result = {}
-    for f in files:
-        print ('Extracting features from image %s' % f)
-        name = f.split('/')[-1].lower()
-        result[name] = extract_features(f)
-    
-    # saving all our feature vectors in pickled file
-    with open(pickled_db_path, 'wb') as fp:
-        pickle.dump(result, fp)
+import extractor
 
 def dotproduct(v1, v2):
     # if len(v1) >= len(v2): length = len(v2)
@@ -67,7 +23,6 @@ def euclidean_dist(sample_vector, test_data_vector):
     vector_dist = norm(sample_vector - test_data_vector)
     return vector_dist
     
-
 def cos_sim(sample_vector, test_data_vector):
     cos_value = dotproduct(sample_vector, test_data_vector)/(norm(sample_vector)*norm(test_data_vector))
     return cos_value
@@ -85,7 +40,7 @@ class Matcher(object):
         self.names = np.array(self.names)
 
     def match(self, image_path, method, topn):
-        sample = extract_features(image_path)
+        sample = extractor.extract_features(image_path)
         result = []
         for i in self.matrix:
             if method == 1:
@@ -108,7 +63,9 @@ def show_img(path):
 def main(method, T, sample):
     datauji_path = 'datauji/'
     files = [os.path.join(datauji_path, p) for p in sorted(os.listdir(datauji_path))]
-    # batch_extractor(datauji_path)
+    
+    extractor.batch_extractor(datauji_path)
+    
     datauji = Matcher('datauji.pck')
     
     print ('Query image ==========================================')
