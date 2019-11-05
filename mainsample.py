1b49b1d91343985ed037ade3867edda48631227d
tonyeko <tonyeko@users.noreply.github.com>
2019-11-05 20:35:04 +0700

    GUI sudah lumayan

--- mainsample.py
+++ mainsample.py
@@ -1,5 +1,6 @@
 import samplefacerecog
 import os
+import tkinter.ttk
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 
@@ -21,21 +22,52 @@ sample = StringVar()
 T = IntVar()
 choice = IntVar()
 
-filelabel = Label(window, text = "Nama File Sample: ").grid(row = 0, column = 0, padx = 0, pady = 10) # this is placed in 0 0
+text1 = r"""
+
+ _______    ___       ______  _______                                                                     
+|   ____|  /   \     /      ||   ____|                                                                    
+|  |__    /  ^  \   |  ,----'|  |__                                                                       
+|   __|  /  /_\  \  |  |     |   __|                                                                      
+|  |    /  _____  \ |  `----.|  |____                                                                     
+|__|   /__/     \__\ \______||_______|                                                                    
+                                                                                                          
+.______       _______   ______   ______     _______ .__   __.  __  .___________. __    ______   .__   __. 
+|   _  \     |   ____| /      | /  __  \   /  _____||  \ |  | |  | |           ||  |  /  __  \  |  \ |  | 
+|  |_)  |    |  |__   |  ,----'|  |  |  | |  |  __  |   \|  | |  | `---|  |----`|  | |  |  |  | |   \|  | 
+|      /     |   __|  |  |     |  |  |  | |  | |_ | |  . `  | |  |     |  |     |  | |  |  |  | |  . `  | 
+|  |\  \----.|  |____ |  `----.|  `--'  | |  |__| | |  |\   | |  |     |  |     |  | |  `--'  | |  |\   | 
+| _| `._____||_______| \______| \______/   \______| |__| \__| |__|     |__|     |__|  \______/  |__| \__| 
+                                                                                                          
+
+                                                                                                By: UNITY
+                                                                                 Tony Eko Yuwono 13518030
+                                                                                  Tifany Angelia 13518067
+                                                                          Rifaldy Aristya Kelana 13518082
+"""
+
+
+style = ttk.Style()
+style.configure('Style.TButton', font='TkFixedFont')
+
+label = ttk.Label(text=text1, style='Style.TButton')
+
+label.grid(row = 0, column = 0, columnspan = 10, padx = 0, pady = 10)
+
+filelabel = Label(window, text = "Nama File Sample: ").grid(row = 14, column = 2, padx = 0, pady = 10) # this is placed in 0 0
 # 'Entry' is used to display the input-field
 filename = Entry(window, textvariable = sample)
-filename.grid(row = 0, column = 1) # this is placed in 0 1
+filename.grid(row = 14, column = 3, columnspan = 2) # this is placed in 0 1
 button = Button(window, text="Browse File", command=open_dialog)
-button.grid(row = 0, column = 2, padx = 10)
+button.grid(row = 14, column = 5, padx = 10)
 
-Label(window, text = "T").grid(row = 1, column = 0, padx = 0, pady = 10) # this is placed in 1 0
-Entry(window, textvariable = T).grid(row = 1, column = 1) # this is placed in 1 1
+Label(window, text = "T").grid(row = 15, column = 2, padx = 0, pady = 10) # this is placed in 1 0
+Entry(window, textvariable = T).grid(row = 15, column = 3, columnspan = 2) # this is placed in 1 1
 
-Label(window, text = "Metode: ").grid(row = 2, column = 0, padx = 0, pady = 10) # this is placed in 1 0
-Radiobutton(window, text="Cosine Similarity", padx = 20, variable=choice, value=1).grid(row = 2, column = 1)
-Radiobutton(window, text="Euclidean Distance", padx = 20, variable=choice, value=2).grid(row = 2, column = 2)
-
-btn = Button(window, text = 'Run', command = runfacerecog).grid(row = 3, column = 1)
+Label(window, text = "Metode: ").grid(row = 17, column = 2, padx = 0, pady = 10) # this is placed in 1 0
+Radiobutton(window, text="Cosine Similarity", padx = 20, variable=choice, value=1).grid(row = 17, column = 4)
+Radiobutton(window, text="Euclidean Distance", padx = 20, variable=choice, value=2).grid(row = 17, column = 5)
+Label(window).grid(pady = 7)
+btn = Button(window, text = 'Run', command = runfacerecog, height = 1, width = 8).grid(row = 18, column = 4)
 
 window.mainloop()
 
