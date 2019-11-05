60622d72b88fd46ce2fb7e0fc89b2920c103345a
tonyeko <tonyeko@users.noreply.github.com>
2019-11-05 21:20:11 +0700

    Sebelum ditambah Background Img

--- mainsample.py
+++ mainsample.py
@@ -31,7 +31,7 @@ filename.grid(row = 14, column = 3, columnspan = 2) # this is placed in 0 1
 button = Button(window, text="Browse File", command=open_dialog)
 button.grid(row = 14, column = 5, padx = 10)
 
-Label(window, text = "T").grid(row = 15, column = 2, padx = 0, pady = 10) # this is placed in 1 0
+Label(window, text = "Banyaknya wajah yang cocok: ").grid(row = 15, column = 2, padx = 0, pady = 10) # this is placed in 1 0
 Entry(window, textvariable = T).grid(row = 15, column = 3, columnspan = 2) # this is placed in 1 1
 
 Label(window, text = "Metode: ").grid(row = 17, column = 2, padx = 0, pady = 10) # this is placed in 1 0
@@ -41,5 +41,3 @@ Label(window).grid(pady = 7)
 btn = Button(window, text = 'Run', command = runfacerecog, height = 1, width = 8).grid(row = 18, column = 4)
 
 window.mainloop()
-
-# sample = "Aaron Paul124_221.jpg"
